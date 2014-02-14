7278c0130e1e5a944c60c54443780cc30946de4c
KJC <kjc.git@gmail.com>
2014-02-13 23:13:14 -0500

    easy, proper view pager for font page

--- src/com/kjcondron/barkeep/MainActivity.java
+++ src/com/kjcondron/barkeep/MainActivity.java
@@ -1,35 +1,80 @@
 package com.kjcondron.barkeep;
 
-import android.R.bool;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
-import android.widget.ViewSwitcher.ViewFactory;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+	
+	private class BarScrollAdapter extends FragmentStatePagerAdapter {
+		
+		private String[] mBars;
+		private int[] mIds;
+		private int mCount;
+		
+		public BarScrollAdapter(FragmentManager fm, Cursor bars) {
+			super(fm);
+			int pos = 0;
+			mCount = bars.getCount() + 1;
+			mBars = new String[mCount];
+			mIds = new int[mCount];
+			bars.moveToFirst();
+			do
+			{
+				mIds[pos] = bars.getInt(0);
+				mBars[pos++] = bars.getString(1);
+			}
+			while(bars.moveToNext());
+			
+			mBars[pos] = "New Bar";
+		}
+		
+		@Override
+		public Fragment getItem(int pos) {
+			if(pos > mCount) MainActivity.log_message(MainActivity.this, "pos greater than count", "BarScrollAdapter.getItem");
+			BarScrollFragment bsf=  new BarScrollFragment();
+			bsf.setText(mBars[pos]);
+			return bsf;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return mCount;
+		}
+		
+		public int getId(int pos)
+		{
+			return (pos == mCount-1) ? -1 : mIds[pos]; 
+		}
+		
+	}
 	
 	public final static String NEWBAR = "com.kjcondron.barkeep.NEWBAR"; 
 	
 	public static int BARID;	
-	boolean makeNewBar;
+	boolean mMakeNewBar;
+	
+	private ViewPager mPager;
+	private BarScrollAdapter mPagerAdapter;
 		
 	/* Checks if external storage is available for read and write */
 	public static boolean isExternalStorageWritable() {
@@ -88,73 +133,36 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         final DBHelper db = new DBHelper(this);
-        makeNewBar = false;
         setContentView(R.layout.layout_splash);
+        boolean mMakeNewBar = getIntent().getBooleanExtra(NEWBAR, false);
         
-        boolean newBar = getIntent().getBooleanExtra(NEWBAR, false);
-        
-        if( !newBar && db.haveBar() )
+        if( !mMakeNewBar && db.haveBar() )
         {
         	try
         	{
-        		View view = findViewById(R.id.layout_slpash);
-				final TextSwitcher ts = (TextSwitcher) findViewById(R.id.textSwitcher1);
-				final Cursor barCursor = db.getBars();
-				barCursor.moveToFirst();
-				ts.setFactory( new ViewFactory() {
+        		final Cursor barCursor = db.getBars();
+				mPager = (ViewPager) findViewById(R.id.pager);
+				mPagerAdapter = new BarScrollAdapter(getSupportFragmentManager(), barCursor);
+				mPager.setAdapter(mPagerAdapter);  	
+				BARID = mPagerAdapter.getId(0); // start of with item 0 shown
+				mPager.setOnPageChangeListener(new OnPageChangeListener() {
 					
 					@Override
-					public View makeView() {
-						TextView myText = new TextView(MainActivity.this);
-						myText.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
-                        myText.setTextSize(36);
-                        myText.setTextColor(Color.BLUE);
-                        return myText;
+					public void onPageSelected(int arg0) {
+						// TODO Auto-generated method stub
+						BARID = mPagerAdapter.getId(arg0);
+					}
+					
+					@Override
+					public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+					}
+					
+					@Override
+					public void onPageScrollStateChanged(int arg0) {
 					}
 				});
-				
-
-				final GestureDetector gd = new GestureDetector(
-						this,
-						new FrontPageGestureListener(this, ts, barCursor, ts));
-        		
-				view.setOnTouchListener( new OnTouchListener() {
-        			boolean displayingNewBar = false;
-					@Override
-					public boolean onTouch(View v, MotionEvent event) {
-						// TODO Auto-generated method stub
-						switch(event.getActionMasked())
-						{
-						case MotionEvent.ACTION_UP:
-							try
-							{
-								if(displayingNewBar)
-								{
-									ts.setText( "New Bar" );
-									makeNewBar = true;
-									displayingNewBar = false;
-									barCursor.moveToFirst();
-								}
-								else
-								{
-									ts.setText( barCursor.getString(1) );
-									BARID = barCursor.getInt(0);
-									makeNewBar = false;
-									if(!barCursor.moveToNext())
-										displayingNewBar = true;
-								}
-							}
-							catch(Exception e)
-							{
-								MainActivity.log_exception(MainActivity.this, e, "TouchListener");
-							}
-							return true;
-						
-						}
-						return gd.onTouchEvent(event);
-					}
-				});		
-        	}
+			}
         	catch(Exception e)
         	{
         		BARID=1;
@@ -162,7 +170,6 @@ public class MainActivity extends Activity {
             	finish();
             	return;
         	}
-        	 
         }
         else
         	makeNewBar();
@@ -210,7 +217,7 @@ public class MainActivity extends Activity {
     }
     
     public void startUse(View view) {
-    	if(makeNewBar)
+    	if(mMakeNewBar || BARID == -1)
     		makeNewBar();
     	else
     		startMyActvity(UseActivity.class);
